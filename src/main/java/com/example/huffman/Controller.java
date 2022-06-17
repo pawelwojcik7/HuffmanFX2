80d274b2cda4126a8a887dd450484235925d67f4
pawel.wojcik7 <pawel.wojcik7@pollub.edu.pl>
2022-06-17 18:24:13 +0200

    Huffmanfx10

--- src/main/java/com/example/huffman/Controller.java
+++ src/main/java/com/example/huffman/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import static com.example.huffman.Huffman.Main_Build_HuffmanTree;
 
@@ -18,7 +19,7 @@ public class Controller {
     @FXML
     private TextArea textArea2;
     @FXML
-    private TableView tableView;
+    private TableView tableView=new TableView<Data>();
      public void huffmanMethod(ActionEvent e)
     {
         System.out.println("tu jestem");
@@ -27,13 +28,32 @@ public class Controller {
         TextManipulator tm = new TextManipulator(word);
         Main_Build_HuffmanTree(tm);
         data = tm.tab;
-        tableView=new TableView<>();
-        TableColumn
-        tableView.getColumns().add("id");
-        tableView.getColumns().add("char");
-        tableView.getColumns().add("weight");
-        tableView.getColumns().add("probability");
-        tableView.getColumns().add("code");
+        Data[] wiersze = new Data[tm.uniqueList.size()];
+        tableView=new TableView<Data>();
+        TableColumn column1 = new TableColumn<Data, String>("Id");
+        column1.setCellValueFactory(new PropertyValueFactory<>("id"));
+        TableColumn column2 = new TableColumn<Data, String>("Character");
+        column2.setCellValueFactory(new PropertyValueFactory<>("character"));
+        TableColumn column3 = new TableColumn<Data, String>("Weight");
+        column3.setCellValueFactory(new PropertyValueFactory<>("weight"));
+        TableColumn column4 = new TableColumn<Data, String>("Probability");
+        column4.setCellValueFactory(new PropertyValueFactory<>("probability"));
+        TableColumn column5 = new TableColumn<Data, String>("Code");
+        column5.setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        tableView.getColumns().add("column1");
+        tableView.getColumns().add("column2");
+        tableView.getColumns().add("column3");
+        tableView.getColumns().add("column4");
+        tableView.getColumns().add("column5");
+        //for(int i=0;i<tm.uniqueList.size();i++)
+        //{
+        //    wiersze[i]=new Data(data[i][0],data[i][1],data[i][2],data[i][3],data[i][4]);
+       //     tableView.getItems().add(wiersze[i]);
+      //  }
+
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
         //table = new JTable(data, columns);
 
 
---
+++ src/main/java/com/example/huffman/Data.java
@@ -0,0 +1,57 @@
+package com.example.huffman;
+
+public class Data {
+    private String id;
+    private String character;
+    private String weight;
+    private String probability;
+    private String code;
+
+    public Data(String id, String character, String weight, String probability, String code) {
+        this.id = id;
+        this.character = character;
+        this.weight = weight;
+        this.probability = probability;
+        this.code = code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getProbability() {
+        return probability;
+    }
+
+    public void setProbability(String probability) {
+        this.probability = probability;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
